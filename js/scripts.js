1967a540d5014c8845e557c1ff29f5aa7bac3525
Lauren Posey <mrslaurenposey@gmail.com>
2016-06-10 13:46:25 -0700

    add frontend and backend logic everything works

--- js/scripts.js
+++ js/scripts.js
@@ -21,5 +21,14 @@ $(document).ready(function(){
   $("form#pingpong-form").submit(function(event){
     event.preventDefault();
 
+    var numbers = getPonged($('#numbers').val());
+
+    numbers.forEach(function(item) {
+      $('#returnResult').append('<li>' + item + '</li>');
+    });
+    $('#returnResult').append(numbers);
+    $("#result").show();
+    $("#pingpong-form").toggle();
+
   });
 });
